1a4507d5fa8b9a6f997b99f49cd7db43bc479178
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 06:23:42 +0200

    [UPD] HOME E ROTTA BACHECA AGGIORNATI

--- app.js
+++ app.js
@@ -3,40 +3,43 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res) =>{
-   const people = [{
-    title : 'Ciambellone',
-    content : 'Il classico ciambellone con yogurt',
-    image : 'imgs/bacheca/ciambellone.jpeg', 
-    tags : ["javascript", "node", "blog"]
-   
-   },
-   {
-    title : 'Cracker barbabietola',
-    content : 'Il classico salatino con barbabietola',
-    image : 'imgs/bacheca/cracker_barbabietola.jpeg', 
-    tags : ["javascript", "node", "blog"]
-   },
-   {
-    title : 'Pane fritto dolce',
-    content : 'Il pane sia dolce ma anche fritto',
-    image : 'imgs/bacheca/pane_fritto_dolce.jpeg', 
-    tags : ["javascript", "node", "blog"]
-   },
-   {
-    title : 'Pasta barbabietola',
-    content : 'la pasta con la barbabietola',
-    image : 'imgs/bacheca/pasta_barbabietola.jpeg', 
-    tags : ["javascript", "node", "blog"]
-   },
-   {
-    title : 'Torta paesana',
-    content : 'la torta di paese',
-    image : 'imgs/bacheca/torta_paesana.jpeg', 
-    tags : ["javascript", "node", "blog"]
-   },
-]
+   res.send('Server del mio blog');
+})
 
-   res.json(people);
+app.get('/bacheca',(req,res) =>{
+    const people = [
+        {
+        title : 'Ciambellone',
+        content : 'Il classico ciambellone con yogurt',
+        image : 'imgs/bacheca/ciambellone.jpeg', 
+        tags : ["javascript", "node", "blog"]
+        },
+       {
+        title : 'Cracker barbabietola',
+        content : 'Il classico salatino con barbabietola',
+        image : 'imgs/bacheca/cracker_barbabietola.jpeg', 
+        tags : ["javascript", "node", "blog"]
+       },
+       {
+        title : 'Pane fritto dolce',
+        content : 'Il pane sia dolce ma anche fritto',
+        image : 'imgs/bacheca/pane_fritto_dolce.jpeg', 
+        tags : ["javascript", "node", "blog"]
+       },
+       {
+        title : 'Pasta barbabietola',
+        content : 'la pasta con la barbabietola',
+        image : 'imgs/bacheca/pasta_barbabietola.jpeg', 
+        tags : ["javascript", "node", "blog"]
+       },
+       {
+        title : 'Torta paesana',
+        content : 'la torta di paese',
+        image : 'imgs/bacheca/torta_paesana.jpeg', 
+        tags : ["javascript", "node", "blog"]
+       },
+    ]
+    res.json(people);
 })
 
 app.listen(port,() =>{
