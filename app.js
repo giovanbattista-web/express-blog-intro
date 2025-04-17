430d62b312287ed145816561c82e066a1db4871e
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 08:57:14 +0200

    [UPD] CORREZIONE ALL'IMAGE DEGLI ARRAY DI OGGETTI

--- app.js
+++ app.js
@@ -5,39 +5,39 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/',(req,res) =>{
-   res.send('Server del mio blog');
-})
+    res.send('Server del mio blog');
+   })
 
 app.get('/bacheca',(req,res) =>{
     const people = [
         {
         title : 'Ciambellone',
         content : 'Il classico ciambellone con yogurt',
-        image : 'imgs/bacheca/ciambellone.jpeg', 
+        image : 'imgs/bacheca/ciambellone1.jpeg', 
         tags : ["javascript", "node", "blog"]
         },
        {
         title : 'Cracker barbabietola',
         content : 'Il classico salatino con barbabietola',
-        image : 'imgs/bacheca/cracker_barbabietola.jpeg', 
+        image : 'imgs/bacheca/cracker_barbabietola1.jpeg', 
         tags : ["javascript", "node", "blog"]
        },
        {
         title : 'Pane fritto dolce',
         content : 'Il pane sia dolce ma anche fritto',
-        image : 'imgs/bacheca/pane_fritto_dolce.jpeg', 
+        image : 'imgs/bacheca/pane_fritto_dolce1.jpeg', 
         tags : ["javascript", "node", "blog"]
        },
        {
         title : 'Pasta barbabietola',
         content : 'la pasta con la barbabietola',
-        image : 'imgs/bacheca/pasta_barbabietola.jpeg', 
+        image : 'imgs/bacheca/pasta_barbabietola1.jpeg', 
         tags : ["javascript", "node", "blog"]
        },
        {
         title : 'Torta paesana',
         content : 'la torta di paese',
-        image : 'imgs/bacheca/torta_paesana.jpeg', 
+        image : 'imgs/bacheca/torta_paesana1.jpeg', 
         tags : ["javascript", "node", "blog"]
        },
     ]
