5580f678eeec68ada18588801a37584ad8aeef37
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 05:46:07 +0200

    [UPD] ARRAY DI OGGETTI

--- app.js
+++ app.js
@@ -3,7 +3,40 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res) =>{
-    res.send('Server del mio blog');
+   const people = [{
+    title : 'Ciambellone',
+    content : 'Il classico ciambellone con yogurt',
+    image : 'imgs/bacheca/ciambellone.jpeg', 
+    tags : ["javascript", "node", "blog"]
+   
+   },
+   {
+    title : 'Cracker barbabietola',
+    content : 'Il classico salatino con barbabietola',
+    image : 'imgs/bacheca/cracker_barbabietola.jpeg', 
+    tags : ["javascript", "node", "blog"]
+   },
+   {
+    title : 'Pane fritto dolce',
+    content : 'Il pane sia dolce ma anche fritto',
+    image : 'imgs/bacheca/pane_fritto_dolce.jpeg', 
+    tags : ["javascript", "node", "blog"]
+   },
+   {
+    title : 'Pasta barbabietola',
+    content : 'la pasta con la barbabietola',
+    image : 'imgs/bacheca/pasta_barbabietola.jpeg', 
+    tags : ["javascript", "node", "blog"]
+   },
+   {
+    title : 'Torta paesana',
+    content : 'la torta di paese',
+    image : 'imgs/bacheca/torta_paesana.jpeg', 
+    tags : ["javascript", "node", "blog"]
+   },
+]
+
+   res.json(people);
 })
 
 app.listen(port,() =>{
